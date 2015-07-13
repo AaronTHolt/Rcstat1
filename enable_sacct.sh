cb19a5b7c09f47188bd76792f6026065e060d215
AaronTHolt <holtat@colorado.edu>
2015-07-13 14:51:09 -0600

    -Remove enable_sacct.sh as it is already incoorperated into starter.py

--- enable_sacct.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export PATH=/curc/slurm/slurm/current/bin:${PATH}
-export LD_LIBRARY_PATH=/curc/slurm/slurm/current/lib:${LD_LIBRARY_PATH}
-export MANPATH=/curc/slurm/slurm/current/share/man:${MANPATH}
-export SLURM_ROOT=/curc/slurm/slurm/current
-export I_MPI_PMI_LIBRARY=/curc/slurm/slurm/current/lib/libpmi.so
